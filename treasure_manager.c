8fc433a1479fef3b24b7899a70af0a36e9c11d09
Natalie Hamdan <nataliehamdan@Natalies-MacBook-Pro.local>
2025-04-11 16:29:09 +0300

    treasure c file

--- treasure_manager.c
+++ treasure_manager.c
@@ -7,14 +7,15 @@
 
 
 typedef struct {
-    char TreasID;
-    char username;
-    float gpsCoo;
-    char clue;
+    char TreasID[30];
+    char username[30];
+    float lat, lon;
+    char clue[100];
     int value;
 }treasure;
 
 int main (int argc, char *argv[]){
+     
     if (argc < 3){
         printf("error: missing arguments\n");
         return 1;
@@ -24,10 +25,11 @@ int main (int argc, char *argv[]){
         char treasures_path[50];
         char logged_hunt_path[50];
         int treasures_fd,logged_fd;
-
+        //add hunt operation
         if (mkdir(hunt_id, 0755) == -1){
             if (errno == EEXIST){
                 printf("directory '%s' already exists", hunt_id);
+                return 1;
             }
             else {
                 perror("mkdir failed");
@@ -37,7 +39,7 @@ int main (int argc, char *argv[]){
         else {
             printf("Directory '%s' created successfully\n", hunt_id);
         }
-
+        //adding 2 empty files inside hunts 
         sprintf(treasures_path, "%s/treasures.bin", hunt_id);
         sprintf(logged_hunt_path, "%s/logged_hunt.txt", hunt_id);
 
@@ -52,9 +54,39 @@ int main (int argc, char *argv[]){
             printf("failed to create logged_hunt file\n");
             return 1;
         }
+        //operations for adding treasure info from STDIN to the files
+        //making a new treasure struct
+        treasure new_treasure;
+        //read data from stdin
+        printf("Add treasure ID:\n");
+        scanf("%s", &new_treasure.TreasID[30]);
+        printf("Add your username:\n");
+        scanf("%s", &new_treasure.username[30]);
+        printf("Add GPS cooridnates (in this format 00.00):\n");
+        printf("Latitude: "); 
+        scanf("%f", &new_treasure.lat);
+        printf("Longitude: "); 
+        scanf("%f", &new_treasure.lon);
+        printf("Add your clue:\n ");
+        scanf("%s", &new_treasure.clue[100]); // problem with clue: scanf encounters empty line and stops 
+        printf("Add value(number): \n");
+        scanf("%d",&new_treasure.value);
+        
+        //use seek_end to go to end of the file 
+        off_t go_to_end = lseek(treasures_fd,0, SEEK_END); //file descriptor of file u want to open, at what byte to position file pointer 0 for end of file, seek end with 0 positions it at end of file 
+        //the actual writing in the file and storng the size 
+        ssize_t bytes_written = write(treasures_fd, &new_treasure, sizeof(treasure) );
+        //a check if write operation worked by comparing sizes
+        if (bytes_written != sizeof(treasure)){
+            printf("Error writing in file\n");
+            return 1;
+        }
+
     
         close(treasures_fd);
         close(logged_fd);
+
+        
     }
     
 
